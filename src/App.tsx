d3d2c26cc6332c672de2981acb5a3d3cac74172c
kelani34 <kelanitaiwo34@gmail.com>
2023-09-11 18:52:30 +0100

    App: create

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import UserProvider from "./auth/AuthProvider";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </Router>
+      </UserProvider>
+    </>
+  );
+}
+
+export default App;
